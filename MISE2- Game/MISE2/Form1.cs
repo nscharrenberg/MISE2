d3cbb33b8e31461093bccf5eb7ea4e253711075f
nscharrenberg <n.scharrenberg@student.fontys.nl>
2017-03-19 16:47:07 +0100

    Draw Cell on Form

--- MISE2- Game/MISE2/Form1.cs
+++ MISE2- Game/MISE2/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MISE2.Assets;
+using MISE2.Assets.enums;
 
 namespace MISE2
 {
@@ -20,16 +21,26 @@ namespace MISE2
 
         private void gamePic_Paint(object sender, PaintEventArgs e)
         {
+            // Character Design
             Character ch = new Character();
             Player pl = new Player();
             Enemy en = new Enemy(new Point(75, 35));
+            Cell ce = new Cell(new Point(10, 10), new Point(2, 2), new Size(20, 20) );
+            // Character
             ch.CurrentPosition = new Point(50, 50);
             ch.DrawCharacter(e.Graphics);
 
+            // Player
             pl.CurrentPosition = new Point(70, 50);
             pl.DrawCharacter(e.Graphics);
 
+            // Enemy
             en.DrawCharacter(e.Graphics);
+
+            ce.CellType = CellTypes.Wall;
+            ce.DrawCell(e.Graphics);
+
+
 
         }
     }
--- MISE2- Game/MISE2/Maps/Cell.cs
+++ MISE2- Game/MISE2/Maps/Cell.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using MISE2.Assets.enums;
@@ -10,14 +11,32 @@ namespace MISE2
 {
     class Cell
     {
+        // Properties
         private Point _coordinates;
         private Point _cellPosition;
         private CellTypes _cellType;
+        private Size _cellSize;
 
+        private static Pen penEmpty = new Pen(Color.LightGray, 1);
+        private static Pen penExit = new Pen(Color.Green, 2);
+        private  static Pen penWall = new Pen(Color.Black, 2);
+        private static SolidBrush sbEmpty = new SolidBrush(Color.White);
+        private static SolidBrush sbExit = new SolidBrush(Color.GreenYellow);
+        private static SolidBrush sbWall = new SolidBrush(Color.SaddleBrown);
+
+        public Cell(Point coordinates, Point position, Size cellSize)
+        {
+            this._coordinates = coordinates;
+            this._cellPosition = position;
+            this._cellSize = cellSize;
+            this.CellType = CellTypes.Empty;
+        }
+
+        // Accessors
         public CellTypes CellType
         {
             get { return _cellType; }
-            private set { _cellType = value; }
+            set { _cellType = value; }
         }
 
         public Point CellPosition
@@ -30,6 +49,37 @@ namespace MISE2
         {
             get { return _coordinates; }
             private set { _coordinates = value; }
+        }
+
+        // Methods
+        public void DrawCell(Graphics g)
+        {
+            Pen p;
+            Brush b;
+
+            if (this.CellType == CellTypes.Exit)
+            {
+                p = Cell.penExit;
+                b = Cell.sbExit;
+            } else if (this.CellType == CellTypes.Wall)
+            {
+                p = Cell.penWall;
+                b = Cell.sbWall;
+            }
+            else
+            {
+                p = Cell.penEmpty;
+                b = Cell.sbEmpty;
+            }
+
+            Rectangle rect = new Rectangle(
+                    this._cellSize.Width * this.Coordinates.X,
+                    this._cellSize.Height * this.Coordinates.Y,
+                    this._cellSize.Width,
+                    this._cellSize.Height
+                );
+            g.FillRectangle(b, rect);
+            g.DrawRectangle(p, rect);
         }
     }
 }
