500592c04bbdd84a87ef9dbb01f088d24b46fc90
nscharrenberg <n.scharrenberg@student.fontys.nl>
2017-03-20 18:09:02 +0100

    Added a Randomize class, to handle map generation better with random values

---
+++ MISE2- Game/MISE2/Randomize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISE2
+{
+    public static class Randomize
+    {
+        static Random rand = new Random();
+
+        public static int Next()
+        {
+            return Randomize.rand.Next();
+        }
+
+        public static int Next(int value)
+        {
+            return Randomize.rand.Next(value);
+        }
+
+        public static int Next(int min, int max)
+        {
+            return Randomize.rand.Next(min, max);
+        }
+    }
+}
